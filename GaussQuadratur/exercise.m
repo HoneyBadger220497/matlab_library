e7cefb0e9336ed8e2381c15dffc99277c6eeeecf
Philipp Schreiner <philipp.schreiner.0203@gmail.com>
2020-01-28 18:36:00 +0100

    add visualization

--- GaussQuadratur/exercise.m
+++ GaussQuadratur/exercise.m
@@ -7,7 +7,21 @@ for k = 1:16
     err(k) = abs(I_exact - I_app(k));
 end
 
-semilogy(err,'o')
-xlabel('St\"{u}tzstellen, $n$','Interpreter','latex','FontSize',15)
-ylabel('$|I - I_n|$','Interpreter','latex','FontSize',15)
+mat_res = abs(log(3) - integral(f,-1,1));
+
+l = semilogy(err,'x');
+hold on
+semilogy([0,17],[1,1]*mat_res)
+set(gca,'FontSize',15)
+xlabel('St\"{u}tzstellen, $n$','Interpreter','latex','FontSize',20)
+ylabel('$|I - I_n|$','Interpreter','latex','FontSize',20)
+xlim([0,17])
+legend('Gauss Quadratur','Matlab Routine')
+
+set(l,'MarkerSize',9)
 grid on
+hold off
+
+I_exact = log(3);
+[I_app, err] = deal(zeros(1,16));
+f = @(x) 1./(x+2);
